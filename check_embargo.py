339b888678ceaf02f9d8c06b075046647178b0a0
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-04-23 16:44:08 +0200

    Update SKIP_CODE

--- check_embargo.py
+++ check_embargo.py
@@ -6,10 +6,12 @@ from zoneinfo import ZoneInfo
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
+SKIP_CODE = 42
+
 def check_embargo_timestamp(file_path: str):
     if not os.path.exists(file_path):
         logging.info(f"Embargo file {file_path} does not exist. Skipping.")
-        sys.exit(0)
+        sys.exit(SKIP_CODE)
 
     with open(file_path, "r") as f:
         embargo_datetime_str = f.readline().strip()
@@ -19,7 +21,7 @@ def check_embargo_timestamp(file_path: str):
         embargo_datetime = datetime.fromisoformat(embargo_datetime_str)
     except ValueError as e:
         logging.info(f"Invalid timestamp format: {e}. Skipping.")
-        sys.exit(0)
+        sys.exit(SKIP_CODE)
 
     if embargo_datetime.tzinfo is None:
         logging.info("Datetime string is naive. Adding timezone Europe/Zurich.")
@@ -32,11 +34,11 @@ def check_embargo_timestamp(file_path: str):
 
     if delta > timedelta(hours=24):
         logging.info(f"Embargo timestamp is older than 24 hours. Skipping.")
-        sys.exit(0)
+        sys.exit(SKIP_CODE)
 
     if delta < 0:
         logging.info(f"Embargo timestamp is in future. Skipping.")
-        sys.exit(0)
+        sys.exit(SKIP_CODE)
 
     logging.info("Embargo timestamp is within the last 24 hours. Proceeding.")
 
