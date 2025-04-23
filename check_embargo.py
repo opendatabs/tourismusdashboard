fe08ebb4804423aa787e9c3bc9315ba13e1aff32
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-04-23 15:52:07 +0200

    Adapt to new script

--- check_embargo.py
+++ check_embargo.py
@@ -6,10 +6,12 @@ from zoneinfo import ZoneInfo
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
-def check_embargo_timestamp(file_path: str):
+def check_embargo_timestamp(file_path: str, output_flag_path: str):
     if not os.path.exists(file_path):
         logging.info(f"Embargo file {file_path} does not exist. Skipping.")
-        sys.exit(0)
+        with open(output_flag_path, "w") as f:
+            f.write("false")
+        return
 
     with open(file_path, "r") as f:
         embargo_datetime_str = f.readline().strip()
@@ -19,30 +21,28 @@ def check_embargo_timestamp(file_path: str):
         embargo_datetime = datetime.fromisoformat(embargo_datetime_str)
     except ValueError as e:
         logging.info(f"Invalid timestamp format: {e}. Skipping.")
-        sys.exit(0)
+        with open(output_flag_path, "w") as f:
+            f.write("false")
+        return
 
     if embargo_datetime.tzinfo is None:
-        logging.info("Datetime string is naive. Adding timezone Europe/Zurich.")
         embargo_datetime = embargo_datetime.replace(tzinfo=ZoneInfo("Europe/Zurich"))
 
     now_in_ch = datetime.now(timezone.utc).astimezone(ZoneInfo("Europe/Zurich"))
     delta = now_in_ch - embargo_datetime
 
-    logging.info(f"Delta is {delta}")
-
-    if delta > timedelta(hours=24):
-        logging.info(f"Embargo timestamp is older than 24 hours. Skipping.")
-        sys.exit(0)
-
-    if delta < 0:
-        logging.info(f"Embargo timestamp is in future. Skipping.")
-        sys.exit(0)
-
-    logging.info("Embargo timestamp is within the last 24 hours. Proceeding.")
+    if delta > timedelta(hours=24) or delta < timedelta(seconds=0):
+        logging.info("Embargo not valid (too old or in future).")
+        with open(output_flag_path, "w") as f:
+            f.write("false")
+    else:
+        logging.info("Embargo is valid.")
+        with open(output_flag_path, "w") as f:
+            f.write("true")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python check_embargo.py <path_to_embargo_file>")
+    if len(sys.argv) != 3:
+        print("Usage: python check_embargo.py <path_to_embargo_file> <path_to_output_flag>")
         sys.exit(1)
 
-    check_embargo_timestamp(sys.argv[1])
+    check_embargo_timestamp(sys.argv[1], sys.argv[2])
