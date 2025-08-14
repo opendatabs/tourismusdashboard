430a3f4c29142916817955b5e9c6fd70aba592a6
Benjamin Marti <benjamin.marti@bs.ch>
2025-08-14 08:31:11 +0200

    Events Juli angepasst

--- app_load_from_OGD.R
+++ app_load_from_OGD.R
@@ -41,6 +41,10 @@ tourismus_events <- tourismus_events %>%
   bind_rows(data.frame(
     Datum = as.Date(c("2025-06-05", "2025-06-06", "2025-06-07")),
     Event = rep("WordCamp Europe 2025 (Informatikveranstaltung)", 3)
+  )) %>% 
+  bind_rows(data.frame(
+    Datum = as.Date(c(paste0("2025-07-0", c(2:9)), paste0("2025-07-", c(10:27)), "02-07-2025", "08-07-2025", "13-07-2025", "19-07-2025", "27-07-2025")),
+    Event = c(rep("UEFA Women's EURO 2025 in Basel", 26), "UEFA Women's Euro 2025 (Schweiz - Norwegen)", "UEFA Women's Euro 2025 (Deutschland - Dänemark)", "UEFA Women's EURO 2025 (Niederlande - Frankreich)", "UEFA Women's EURO 2025 (Viertelfinale Frankreich – Deutschland)", "UEFA Women's EURO 2025 (Finale England – Spanien)")
   ))
 
 # load data for tourismus and left join with events: ####
